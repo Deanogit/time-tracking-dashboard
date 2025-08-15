ac71772f54b4ca3bf6f8f6053b82f574de77f47f
Dean Howe <hideanhowe@gmail.com>
2025-08-15 16:50:09 +0100

    padding

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const data = await getAllCards();
   console.log(data);
   return (
-    <div className="bg-[var(--cust-navy-950)] grid min-h-[100vh] justify-center items-center">
+    <div className="bg-[var(--cust-navy-950)] grid gap-6 min-h-[100vh] justify-center items-center py-20">
       <Header />
       {data.map((card) => (
         <CardEl key={card.title} card={card} />
